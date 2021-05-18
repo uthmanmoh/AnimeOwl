d4dc07f7a10c1a108351d183ea246102779b77c4
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2021-05-18 00:35:07 -0400

    Initial Commit

---
+++ AnimeOwl/AnimeOwlApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeOwlApp.swift
+//  AnimeOwl
+//
+//  Created by Uthman Mohamed on 2021-05-18.
+//
+
+import SwiftUI
+
+@main
+struct AnimeOwlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
